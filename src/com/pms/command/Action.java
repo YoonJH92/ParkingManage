c21f1ad2f839cc4fe874cf786baf9d4312cd2acd
YJH <wngks745@naver.com>
2020-08-25 12:14:25 +0900

    20200825 서블릿추가

---
+++ src/com/pms/command/Action.java
@@ -0,0 +1,30 @@
+package com.pms.command;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Action {
+	private static Action instance = new Action();
+	
+	public static Action getInstance() {
+		return instance;
+	}
+
+	private HashMap<String, Command> map = new HashMap<String, Command>();
+	
+	public Action() {
+		// 페이지 추가시 마다 추가바람
+		map.put("/stat/daily", new StatDailyCommand());
+		map.put("/empty", new EmptyCommand());
+		
+	}
+	
+	public Command getAction(String command) {
+		Command action = null;
+		action = map.get(command);
+		if(action == null) {
+			action = map.get("/empty");
+		}
+		return action;
+	}
+}
