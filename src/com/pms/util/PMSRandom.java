7bce78564a014064b84c1d955994f87349630ff0
YJH <wngks745@naver.com>
2020-09-03 16:47:28 +0900

    20200903 random생성

--- src/com/pms/util/PMSRandom.java
+++ src/com/pms/util/PMSRandom.java
@@ -41,33 +41,59 @@ public class PMSRandom {
 		}
 		return CNUM;
 	}
-	
+	//이름 자동생성
 	public static String randomHangulName() {
-	    List<String> first = Arrays.asList("김", "이", "박", "최", "정", "강", "조", "윤", "장", "임", "한", "오", "서", "신", "권", "황", "안",
-	        "송", "류", "전", "홍", "고", "문", "양", "손", "배", "조", "백", "허", "유", "남", "심", "노", "정", "하", "곽", "성", "차", "주",
-	        "우", "구", "신", "임", "나", "전", "민", "유", "진", "지", "엄", "채", "원", "천", "방", "공", "강", "현", "함", "변", "염", "양",
-	        "변", "여", "추", "노", "도", "소", "신", "석", "선", "설", "마", "길", "주", "연", "방", "위", "표", "명", "기", "반", "왕", "금",
-	        "옥", "육", "인", "맹", "제", "모", "장", "남", "탁", "국", "여", "진", "어", "은", "편", "구", "용");
+	    List<String> first = Arrays.asList("김", "이", "박", "최", "정", "강", "조", "윤", "장", "임", "한", "오", "서", "신", "권", "황", "안");
 	    List<String> name = Arrays.asList("가", "강", "건", "경", "고", "관", "광", "구", "규", "근", "기", "길", "나", "남", "노", "누", "다",
 	        "단", "달", "담", "대", "덕", "도", "동", "두", "라", "래", "로", "루", "리", "마", "만", "명", "무", "문", "미", "민", "바", "박",
 	        "백", "범", "별", "병", "보", "빛", "사", "산", "상", "새", "서", "석", "선", "설", "섭", "성", "세", "소", "솔", "수", "숙", "순",
 	        "숭", "슬", "승", "시", "신", "아", "안", "애", "엄", "여", "연", "영", "예", "오", "옥", "완", "요", "용", "우", "원", "월", "위",
-	        "유", "윤", "율", "으", "은", "의", "이", "익", "인", "일", "잎", "자", "잔", "장", "재", "전", "정", "제", "조", "종", "주", "준",
-	        "중", "지", "진", "찬", "창", "채", "천", "철", "초", "춘", "충", "치", "탐", "태", "택", "판", "하", "한", "해", "혁", "현", "형",
-	        "혜", "호", "홍", "화", "환", "회", "효", "훈", "휘", "희", "운", "모", "배", "부", "림", "봉", "혼", "황", "량", "린", "을", "비",
-	        "솜", "공", "면", "탁", "온", "디", "항", "후", "려", "균", "묵", "송", "욱", "휴", "언", "령", "섬", "들", "견", "추", "걸", "삼",
-	        "열", "웅", "분", "변", "양", "출", "타", "흥", "겸", "곤", "번", "식", "란", "더", "손", "술", "훔", "반", "빈", "실", "직", "흠",
-	        "흔", "악", "람", "뜸", "권", "복", "심", "헌", "엽", "학", "개", "롱", "평", "늘", "늬", "랑", "얀", "향", "울", "련");
+	        "유", "윤", "율", "으", "은", "의", "이", "익");
 	    Collections.shuffle(first);
 	    Collections.shuffle(name);
 	    return first.get(0) + name.get(0) + name.get(1);
 	  }
 
+	//이메일 자동생성
+	public static String randomEmailName() {
+	    List<String> name = Arrays.asList("a","b","c","d","e","f","g","h","1","2","3","4","5","6","7","8","9");
+	    List<String> addr = Arrays.asList("@naver.com","@gmail.com","@nate.com");
+	    Collections.shuffle(addr);
+	    Collections.shuffle(name);
+	    
+	    StringBuffer buff = new StringBuffer();
+	    
+	    for(int i = 0 ; i < 6 ; i++) {
+	    	buff.append(name.get(i));
+	    }
+	    return buff.toString() + addr.get(0);
+	  }
+	
+	//전화번호 자동생성
+	public static String randomPhoneName() {
+	    List<String> name = Arrays.asList("0","1","2","3","4","5","6","7","8","9");
+	    Collections.shuffle(name);
+	    
+	    StringBuffer buff = new StringBuffer();
+	    
+	    for(int i = 0 ; i < 8 ; i++) {
+	    	buff.append(name.get(i));
+	    }
+	    buff.insert(4, "-");
+	    
+	    return "010-"+buff.toString();
+	  }
+	
+	// 타입 자동생성
+	public static String randomPhoneType() {
+	    List<String> name = Arrays.asList("관리자","일반","일반","일반","일반","일반","일반","일반","일반");
+	    Collections.shuffle(name);
+	    return name.get(0);
+	  }
+	
 
 	// 로그 생성
 	public void TIME_SETTING(ArrayList<String> CNUM , int count) {
-		Random rand = new Random();
-		
 		String startDate = "2020-5-1 00:00:00";
 		String endDate = "2020-9-10 00:00:00";
 		Timestamp stime = Timestamp.valueOf(startDate); 
@@ -90,42 +116,42 @@ public class PMSRandom {
 			Collections.sort(rand_time_arr);
 			map.put(CNUM.get(i), rand_time_arr);
 		}
-		
+
 		Set<String> keys = map.keySet();
 		String in = null;
 		String out = null;
-		long st = 0;
-		long gt = 0;
+		long endTime = 0;
+		int num = 0;
 		RandomInsert randomInsert = new RandomInsert();
 		for (String key : keys) {
 		  for(int i = 0; i < map.get(key).size(); i++) {
+			  num = 0;
 			  SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			  
-			  gt = map.get(key).get(i);
-			  
-			  if(gt > st && i > 0) {
-				  continue;
+
+			  if(i > 0 ) {
+				  if(endTime > map.get(key).get(i)) {
+					  continue;
+				  } 
 			  }
 			  
-			  rand_diff = (long)(Math.random() * 86400000) + 43200000;
-			  st = gt + rand_diff;			  
+			  rand_diff = (long)(Math.random() * 43200000) + 86400000;
+			  endTime = map.get(key).get(i) + rand_diff;			  
 			  
 			  in = format.format(new Date(map.get(key).get(i)));
 
 			  if(i < map.get(key).size()-1) {
-				  out = format.format(new Date(st));
+				  out = format.format(new Date(endTime));
 			  }else {
 				  out = null;
 			  }
-			  
-			  randomInsert.randomLogAdd(key,in,out);
+			  num = randomInsert.monthNum(key,in,out);
+			  randomInsert.randomLogAdd(key,in,out,num);
 		  }
 		}
 	}
 	
 	//월정액 회원 생성
 	private void MONTH_SETTING(ArrayList<String> CNUM) {
-		Random rand = new Random();
 		String startDate = "2020-5-1 00:00:00";
 		String endDate = "2020-9-10 00:00:00";
 		Timestamp stime = Timestamp.valueOf(startDate); 
@@ -138,7 +164,7 @@ public class PMSRandom {
 		
 		ArrayList<Long> rand_time_arr = null;
 		
-		for(int i=0; i < (CNUM.size()/5); i++) {
+		for(int i=0; i < (CNUM.size()/2); i++) {
 			rand_time_arr = new ArrayList<Long>();
 			for(int j = 0; j < 4 ; j++) {
 				rand_diff = (long)(Math.random() * diff);
@@ -152,42 +178,37 @@ public class PMSRandom {
 		Calendar cal = Calendar.getInstance(); // stopDate 사용
 
 		Set<String> keys = map.keySet();
-		String in = null;
-		String out = null;
 		String toDate = null;
 		String stopDate = null;
-		long st = 0;
-		long gt = 0;
 		RandomInsert randomInsert = new RandomInsert();
 		for (String key : keys) {
-			String name = randomHangulName();
+			  String name = randomHangulName();
+			  String email = randomEmailName();
+			  String phone = randomPhoneName();
+			  String type = randomPhoneType();
 		  for(int i = 0; i < map.get(key).size(); i++) {
-
-			  
-			  
 			  SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			  cal.setTime(new Date(map.get(key).get(i))); 
 			  cal.add(Calendar.MONTH, 1);	 
-			  //long a = cal.getTime();
-			  //long b = map.get(key).get(i+1);
+			  
 			  if(i < map.get(key).size()-1) {
-				  if(map.get(key).get(i)+(864000000*30) > map.get(key).get(i+1)) {
-					  	continue;
+				  if(map.get(key).get(i+1) > cal.getTimeInMillis() ) {
+					  toDate = format.format(new Date(map.get(key).get(i)));
+					  stopDate = format.format(cal.getTime()); // String 으로 반환
+					  randomInsert.randomMemberAdd(key,toDate,stopDate,name,email,phone,type);
 				  }
-				  toDate = format.format(new Date(map.get(key).get(i)));
-				  stopDate = format.format(cal.getTime()); // String 으로 반환
-				  randomInsert.randomMemberAdd(key,toDate,stopDate,name);
 			  }
-
 		  }
 		}
 	}
 	
 	public static void main(String[] args) {
 		PMSRandom random = new PMSRandom();
-		ArrayList<String> ran = random.CNUM_RAND(300); 
-		//random.TIME_SETTING(ran, 100);
+		ArrayList<String> ran = random.CNUM_RAND(500); 
 		random.MONTH_SETTING(ran);
+		random.TIME_SETTING(ran, 20);
+
+
 		
 		System.out.println("성공");
 	}
